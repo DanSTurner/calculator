4914f2733a837dad2bf93969a1a24c0ee9de70aa
Dan S Turner <turner@nowwithmoredan.com>
2013-11-16 13:59:13 -0800

    Pass test, add new test

--- lib/parser.rb
+++ lib/parser.rb
@@ -3,9 +3,10 @@ class Parser
   def self.parse(input)
     input = input.split
     output = []
-    output << input.pop.to_i
-    output.unshift(input.pop.to_sym)
-    output << input.pop.to_i
+    output.insert(0, input[1].to_sym)
+    output.insert(1, input[0].to_i)
+    output.insert(2, input[2].to_i)
   end
+  # refactor recursively, give up RPN
 
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -10,4 +10,12 @@ class ParserTest < MiniTest::Unit::TestCase
     assert_equal expected, Parser.parse(old)
   end
 
+  def test_handles_more_than_1_operation
+    skip
+    old = "1 + 2 + 3"
+    expected = [:+, [:+, 1, 2], 3]
+
+    assert_equal expected, Parser.parse(old)
+  end
+
 end
