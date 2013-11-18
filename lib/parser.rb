e5ce347313ca2b2af8e90fb6bc140d1a1912c9a4
Dan S Turner <turner@nowwithmoredan.com>
2013-11-18 09:23:22 -0800

    Remove RPN spec

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,11 +2,10 @@ class Parser
 
   def self.parse(input)
     input = input.split
-    output = []
-    output.insert(0, input[1].to_sym)
-    output.insert(1, input[0].to_i)
-    output.insert(2, input[2].to_i)
+    output = Array.new
+    output << input[0].to_i
+    output << input[1].to_sym
+    output << input[2].to_i
   end
-  # refactor recursively, give up RPN
 
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,17 +3,16 @@ require './lib/parser'
 
 class ParserTest < MiniTest::Unit::TestCase
 
-  def test_returns_reverse_polish_notation
+  def test_returns_elements_in_array
     old = "1 + 2"
-    expected = [:+, 1, 2]
+    expected = [1, :+, 2]
 
     assert_equal expected, Parser.parse(old)
   end
 
-  def test_handles_more_than_1_operation
-    skip
-    old = "1 + 2 + 3"
-    expected = [:+, [:+, 1, 2], 3]
+  def test_handles_outside_parens
+    old = "(1 + 2)"
+    expected = expected = [1, :+, 2]
 
     assert_equal expected, Parser.parse(old)
   end
