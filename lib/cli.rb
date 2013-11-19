c93e745e8b223d01271dbaf76f9456bf9a50e3af
Dan S Turner <turner@nowwithmoredan.com>
2013-11-18 16:46:03 -0800

    Add mocks, next step is understand mocks

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,14 @@
+require './lib/calculator'
+
 class CLI
+  def initialize(input = $stdin, output = $stdout)
+    @input = input
+    @output = output
+  end
+
   def run
-    puts "Enter an expression to evaluate:"
+    @output.puts "Enter an expression to evaluate:"
+    a = @input.gets
+    @output.puts Calculator.evaluate(a)
   end
 end
-
-CLI.new.run
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require './lib/cli'
+require 'mocha/setup'
 
 class CLITest < MiniTest::Unit::TestCase
   def setup
@@ -7,6 +8,14 @@ class CLITest < MiniTest::Unit::TestCase
   end
 
   def test_expect_ask_for_expression
-    proc { @CLI.run }.must_output "Enter an expression to evaluate:\n"
+    mock_out = mock('object')
+    mock_in = mock('object')
+    mock_out.expects(:puts).with("Enter an expression to evaluate:")
+    mock_in.expects(:gets).returns("1 + 1")
+    mock_out.expects(:puts).with(2)
+    @CLI = CLI.new(mock_in, mock_out)
+    @CLI.run
   end
+
+
 end
